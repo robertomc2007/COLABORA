4fc9eabd3175a997315c4eb9da197bd28b58a25d
robertomc2007 <robertomc2007@gmail.com>
2016-10-07 15:39:03 -0300

    Add files via upload

---
+++ SENSORHUMEDAD/UNO_HUMEDADSUELO_TEST/UNO_HUMEDADSUELO_TEST.ino
@@ -0,0 +1,21 @@
+/*
+LECTURA DEL SENSOR DE HUMEDAD EN EL SUELO CADA 0,5 SEG
+APTO PARA MOSTRAR POR LA CONSOLA GRAFICA
+ */
+
+int sensorHumedadSuelo = 0;
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the analog in value:
+  sensorHumedadSuelo = analogRead(A0);
+  // map a un rango de salida
+  //int sensorEscalado = map(sensorHumedadSuelo, 0, 1023, 0, 100);
+  Serial.println(sensorHumedadSuelo);
+
+  delay(500);
+}
